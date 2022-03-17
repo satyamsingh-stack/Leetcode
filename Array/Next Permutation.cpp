4950224457bd4a017025f5b7331d68bd01b88720
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-03-17 11:10:33 +0530

    Create Next Permutation.cpp

---
+++ Array/Next Permutation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& arr) {
+        int k, l,N=arr.size();
+    	for (k = N - 2; k >= 0; k--) {
+            if (arr[k] < arr[k + 1]) {
+                break;
+            }
+        }
+    	if (k < 0) {
+    	    reverse(arr.begin(), arr.end());
+    	} else {
+    	    for (l = N - 1; l > k; l--) {
+                if (arr[l] > arr[k]) {
+                    break;
+                }
+            } 
+    	    swap(arr[k], arr[l]);
+    	    reverse(arr.begin() + k + 1, arr.end());
+        }
+    }
+};
