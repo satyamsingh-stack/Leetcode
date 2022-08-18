a653ad50a035650a61f7fd803514fe0c4fb9d3fd
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-08-18 22:08:09 +0530

    Create Reduce array size.cpp

---
+++ Array/Reduce array size.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<arr.size();i++)
+            mp[arr[i]]++;
+        multiset<int,greater<int>> s;
+        for(auto x:mp)
+            s.insert(x.second);
+        int ans=0,count=0;
+        for(auto it=s.begin();count*2<arr.size();it++){
+            ans++;
+            count=count+*it;
+        }
+        return ans;
+    }
+};
